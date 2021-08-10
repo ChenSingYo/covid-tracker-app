e4b3602241e1cee8a6c6211822c983222fde0e06
ChenSingYo <chensingyo@gmail.com>
2021-08-10 17:00:45 +0800

    option api vue 3

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses pr-2"></i>Covid-19 Tracker
+    </div>
+    <p>API By <a class="text-blue-300" href="https://covid19api.com" target="_blank">covid10api.com</a></p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
